75ad4aed40c8ebd8298bc761fb124e15d6910ce2
weihao-Tey <weihowtey@gmail.com>
2024-04-24 09:26:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# removeassigntoclient
+This removes the "Assign to Another Client" Action Link from all cases in CiviCRM.
+
+This is an [extension for CiviCRM](https://docs.civicrm.org/sysadmin/en/latest/customize/extensions/), licensed under [AGPL-3.0](LICENSE.txt).
---
+++ removeassigntoclient.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once 'removeassigntoclient.civix.php';
+
+use CRM_Removeassigntoclient_ExtensionUtil as E;
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function removeassigntoclient_civicrm_config(&$config): void {
+  _removeassigntoclient_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function removeassigntoclient_civicrm_install(): void {
+  _removeassigntoclient_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function removeassigntoclient_civicrm_enable(): void {
+  _removeassigntoclient_civix_civicrm_enable();
+}
+
+
+/**
+ * 
+ * Implements hook_civicrm_links().
+ * 
+ * Removes the ActionLinks if Activity Status is 'Locked'. [Activity Tab]
+ * 
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_links/
+ */
+function removeassigntoclient_civicrm_links($op, $objectName, $objectId, &$links, &$mask, &$values) {
+  // Only modify links for activity listings
+  if ($objectName == 'Case') {
+    // Now, to remove the Edit button/link
+    foreach ($links as $key => $link) {
+      // Check for the Edit link by title or any other identifiable attribute
+      if (isset($link['title']) && $link['title'] == ts('Assign to Another Client')) {
+          unset($links[$key]);
+      }
+    }
+  }
+}
